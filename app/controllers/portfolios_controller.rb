7f80ff62001851976aa01d6e628e1923e590b3d8
Bouare <modibob42@gmail.com>
2022-06-18 06:37:10 +0000

    Create Action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,21 @@
 class PortfoliosController < ApplicationController
-	  def index
-		@pf = Portfolio.all
-	  end
+		def index
+			@pf = Portfolio.all
+		end
+
+		def new
+			@single_pf = Portfolio.new
+		end
+	  
+		def create
+			@single_pf = Portfolio.new(params.require(:portfolio).permit(:tittle, :subtitle, :body))
+
+			respond_to do |format|
+			  if @single_pf.save
+				format.html { redirect_to portfolios_path, notice: "Portfolio was successfully created." }
+			  else
+				format.html { render :new, status: :unprocessable_entity }
+			  end
+			end
+		end
 end
