cf323d19965f66a3732190284aa944e826299a43
Bouare <modibob42@gmail.com>
2022-06-18 04:22:53 +0000

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title:"My BLog title #{blog}",
+		body:"Hello world, I am Modibo and I am still hustling. My magic sentence is: sleep less dream more"
+	)
+end
+
+puts "10 blogs created"
+
+5.times do |skill|
+	Skill.create!(
+		tittle:"My Skill title #{skill}",
+		percent: 97
+	)
+end
+
+puts "5 skills created"
+
+9.times do |pf|
+	Portfolio.create!(
+		tittle:"My portfolio title #{pf}",
+		subtitle: "My service",
+		body: "",
+		main_image: "https://via.placeholder.com/600x400",
+		thumb_image: "https://via.placeholder.com/200x100"
+	)
+end
+
+puts "10 PF created"
